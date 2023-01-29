4d26dbcfcc4338bf717cd9554b32d4025c46e208
nikalex777 <777_nikolaev@mail.ru>
2023-01-29 16:36:21 +0300

    HTTP homeworks

---
+++ 9.HTTP/src/main/java/ru/netology/http/Cat.java
@@ -0,0 +1,31 @@
+package ru.netology.http;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Cat(String id, String text, String type, String user, String upvotes) {
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotse") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post" +
+                "\n  id=" + id +
+                "\n  text=" + text +
+                "\n  type=" + type +
+                "\n  user=" + user +
+                "\n upvotes=" + upvotes;
+    }
+}
+
